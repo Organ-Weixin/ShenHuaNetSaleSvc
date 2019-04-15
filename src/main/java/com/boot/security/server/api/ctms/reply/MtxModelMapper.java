64dc6cb815b708416ae0f21faedafd0fe4196150
ChaoYongGang123 <2441220411@qq.com>
2019-04-15 20:43:11 +0800

    满天星-提交MtxModelMapper-新

--- src/main/java/com/boot/security/server/api/ctms/reply/MtxModelMapper.java
+++ src/main/java/com/boot/security/server/api/ctms/reply/MtxModelMapper.java
@@ -1,8 +1,16 @@
 package com.boot.security.server.api.ctms.reply;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
+
+import com.boot.security.server.model.Screenseatinfo;
+import com.boot.security.server.model.SessionSeat;
+import com.boot.security.server.model.SessionSeatStatusEnum;
 import com.boot.security.server.model.Screeninfo;
+import com.boot.security.server.model.Sessioninfo;
 
 public class MtxModelMapper {
 	 public static Screeninfo MapToEntity(MtxGetHallResult.ResBean.HallsBean.HallBean model, Screeninfo entity)
@@ -12,5 +20,33 @@ public class MtxModelMapper {
          entity.setUpdateTime(new Date());
          return entity;
      }
+	 //影厅对应的所有座位信息转为entity
+	 public static Screenseatinfo MapToEntity(MtxGetHallAllSeatResult.HallAllSeatBean model,Screenseatinfo entity)
+	 {
+		 entity.setSeatCode(model.getSeatNo());
+		 entity.setRowNum(model.getSeatRow());
+		 entity.setColumnNum(model.getSeatCol());
+		 entity.setXCoord(Integer.valueOf(model.getGraphRow()));
+		 entity.setYCoord(Integer.valueOf(model.getGraphCol()));
+		 entity.setGroupCode(model.getSeatPieceNo());
+		 entity.setUpdateTime(new Date());
+		return entity; 
+	 }
+	 //对应影院对应日期的排期转为entity
+	 public static Sessioninfo MapToEntity(MtxGetCinemaPlanResult.ResBean.CinemaPlansBean.CinemaPlanBean model,Sessioninfo entity) throws ParseException
+	 {
+		 entity.setCCode(model.getPlaceNo());
+		 entity.setScreenCode(model.getHallNo());
+		 entity.setFeatureNo(model.getFeatureNo());
+		 entity.setFilmCode(model.getFilmNo());
+		 entity.setFilmName(model.getFilmName());
+		 entity.setLanguage(model.getCopyLanguage());
+		 entity.setLowestPrice(model.getProtectPrice());
+		 entity.setStandardPrice(model.getStandPric());
+		 entity.setUpdateTime(new Date());
+		return entity;
+	 }
 
+	 
+	 
 }
