7caa17ba94d443e54afe74a378fb54d14f33c566
ChaoYongGang123 <2441220411@qq.com>
2019-05-23 11:34:52 +0800

    订单详情-新增sql

--- src/main/java/com/boot/security/server/dao/GoodsorderdetailsviewDao.java
+++ src/main/java/com/boot/security/server/dao/GoodsorderdetailsviewDao.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
+import com.boot.security.server.model.Adminorderview;
 import com.boot.security.server.modelView.Goodsorderdetailsview;
 
 
@@ -15,4 +17,7 @@ public interface GoodsorderdetailsviewDao {
     int count(@Param("params") Map<String, Object> params);
 
     List<Goodsorderdetailsview> list(@Param("params") Map<String, Object> params, @Param("offset") Integer offset, @Param("limit") Integer limit);
+    @Select("select * from  goodsorderdetailsview t where t.cinemacode = #{cinemacode}")
+    List<Goodsorderdetailsview> getByCinemaCode(String cinemacode);
+
 }
