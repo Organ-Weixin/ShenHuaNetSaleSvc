f9b80e4b69fa8e293c43e2454132f5375c1e2026
liuyudong <liuyudong1208@163.com>
2019-06-08 18:48:34 +0800

    orderseatdetailsServiceImpl提交

---
+++ src/main/java/com/boot/security/server/service/impl/OrderseatdetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.boot.security.server.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.boot.security.server.dao.OrderseatdetailsDao;
+import com.boot.security.server.model.Orderseatdetails;
+import com.boot.security.server.service.OrderseatdetailsService;
+@Service
+public class OrderseatdetailsServiceImpl implements OrderseatdetailsService{
+	@Autowired
+	private OrderseatdetailsDao orderseatdetailsDao;
+	
+	@Override
+	public List<Orderseatdetails> getByOrderId(Long orderid) {
+		return orderseatdetailsDao.getByOrderId(orderid);
+	}
+
+	@Override
+	public int update(Orderseatdetails orderseatdetails) {
+		return orderseatdetailsDao.update(orderseatdetails);
+	}
+
+}
