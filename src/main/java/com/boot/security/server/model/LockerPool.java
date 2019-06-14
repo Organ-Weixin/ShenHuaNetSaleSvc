7ac97cfd5aba82d4ec666c265579934f9ccd0987
yangjialong <1443119959@qq.com>
2019-06-14 23:32:32 +0800

    聊天室

---
+++ src/main/java/com/boot/security/server/model/LockerPool.java
@@ -0,0 +1,34 @@
+package com.boot.security.server.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 神画 根据唯一标识创建对象池，给不同业务逻辑单独上锁(实现单例)
+ */
+public class LockerPool {
+    Map<String,String> lockMap = new HashMap<>();
+    private static volatile LockerPool lockerPool = null;
+
+    public String getLocker(String id){
+        if(lockMap.get(id)==null){
+            lockMap.put(id,"Lock"+id);
+        }
+        return lockMap.get(id);
+    }
+
+    private LockerPool(){
+    	
+    }
+
+    public static LockerPool getSingleton(){
+        if(lockerPool == null){
+            synchronized (LockerPool.class){
+                if(lockerPool == null){
+                    lockerPool = new LockerPool();
+                }
+            }
+        }
+        return lockerPool;
+    }
+}
