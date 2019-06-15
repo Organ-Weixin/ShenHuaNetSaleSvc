29337e9a3fbc857a41bd079d185f3e8d33fac6c6
zjm <jianming2032@163.com>
2019-06-15 20:27:06 +0800

    test测试

--- src/main/java/com/boot/security/server/SecurityApplication.java
+++ src/main/java/com/boot/security/server/SecurityApplication.java
@@ -1,5 +1,8 @@
 package com.boot.security.server;
 
+import java.nio.charset.Charset;
+import java.util.List;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
@@ -8,7 +11,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 启动类
@@ -18,10 +25,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @SpringBootApplication
 @EnableTransactionManagement
-public class SecurityApplication {
+public class SecurityApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
-
 }
