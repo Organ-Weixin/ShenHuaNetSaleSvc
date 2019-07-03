b96f85f13c846663013e04e37b0abdaf2294c0dd
zjm <jianming2032@163.com>
2019-07-03 18:47:07 +0800

    goodsordersdao

--- src/main/java/com/boot/security/server/dao/GoodsordersDao.java
+++ src/main/java/com/boot/security/server/dao/GoodsordersDao.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.boot.security.server.model.Goodsorders;
 
